ca73426989e3a7d1299bd2ec6cf57b28eecc17b4
tunt-2001 <trongtu28032001@gmail.com>
2022-07-20 20:17:28 +0700

    zzzzz

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em à!',
-    introDesc: `Trái đất này vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introTitle: '... à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Em đồng ý',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh 🥰',
-    reply: 'Đơn giản bởi vì YÊU',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Cầm điện thoại và gọi cho anh đi.',
-    btnAccept: 'Okiiiii 😘😘',
-    messLink: 'https://www.facebook.com/trongtu201/' 
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, Cô gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://facebook.com/profile.php?id=100011090316948' 
 }
